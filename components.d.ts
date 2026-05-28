ba73cff854f0e2e90df69936980708c7ced32c05
syh053 <syh053@gmail.com>
2026-05-28 14:04:09 +0800

    feat(main): 調整 header 顯示時機

--- components.d.ts
+++ components.d.ts
@@ -11,6 +11,7 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
+    ElAvatar: typeof import('element-plus/es')['ElAvatar']
     ElButton: typeof import('element-plus/es')['ElButton']
     ElCheckbox: typeof import('element-plus/es')['ElCheckbox']
     ElCheckboxGroup: typeof import('element-plus/es')['ElCheckboxGroup']
--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
-import { ref } from 'vue'
 import { userLogOutApi } from "@/api/user"
-import { useRouter } from "vue-router"
+import { useRoute, useRouter } from "vue-router"
+import { computed } from "vue"
 
 const router = useRouter()
+const headerRoute = useRoute()
 
-/* header */
-const activeIndex = ref('1')
-const handleSelect = (key: string, keyPath: string[]) => {
-  console.log(key, keyPath)
-}
+/* 是否顯示 Header */
+const showHeader = computed(() => {
+  return headerRoute.name !== 'logIn' && headerRoute.name !==  'signUp'
+})
 
 const logOut = async () => {
   await userLogOutApi()
@@ -18,19 +18,24 @@ const logOut = async () => {
 </script>
 
 <template>
-  <div class="fixed top-0 left-0 right-0 h-(--header-height) z-9999">
+  <div v-if="showHeader" class="fixed top-0 left-0 right-0 h-(--header-height) z-9999">
     <el-menu
-        :default-active="activeIndex"
         class="el-menu-demo flex justify-end"
         mode="horizontal"
-        @select="handleSelect"
     >
-      <el-menu-item index="1">
-        <router-link :to="{ name: 'logIn' }">登入</router-link>
-      </el-menu-item>
-      <el-menu-item index="2" @click="logOut">
-        登出
-      </el-menu-item>
+
+      <el-sub-menu index="2">
+        <template #title>
+          <el-avatar
+              :size="32"
+              class="mr-3"
+              src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
+          />
+        </template>
+        <el-menu-item index="2" @click="logOut">
+          登出
+        </el-menu-item>
+      </el-sub-menu>
     </el-menu>
   </div>
 
