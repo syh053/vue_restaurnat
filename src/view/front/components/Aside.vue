9325047f724356cba64f179a45d4dea9f2a2031e
syh053 <syh053@gmail.com>
2026-08-24 00:11:09 +0800

    feat(user-store): 將使用者相關的操作一律由 userStore 來處理

--- src/view/front/components/Aside.vue
+++ src/view/front/components/Aside.vue
@@ -1,15 +1,42 @@
 <script setup lang="ts">
+import { ref } from "vue"
+import { Icon } from "@iconify/vue"
 
-
-
+/* 手機版抽屜選單開關 */
+const drawerVisible = ref(false)
 </script>
 
 <template>
-  <el-aside class="h-full border-r border-gray-200 pt-15" width="200px">
+  <!-- 桌面版固定側欄 -->
+  <el-aside class="hidden lg:block h-full border-r border-gray-200 pt-15" width="200px">
     <slot />
   </el-aside>
+
+  <!-- 手機版漢堡選單按鈕 -->
+  <button
+      class="button-hamburger lg:hidden fixed left-3 top-(--header-height) mt-3 z-50 flex items-center justify-center w-10 h-10 rounded-lg bg-white shadow"
+      aria-label="開啟選單"
+      @click="drawerVisible = true"
+  >
+    <Icon icon="material-symbols:menu-rounded" width="22" height="22" />
+  </button>
+
+  <!-- 手機版抽屜選單，點選項目後自動關閉 -->
+  <el-drawer v-model="drawerVisible" direction="ltr" size="200px" :with-header="false">
+    <div class="pt-15" @click="drawerVisible = false">
+      <slot />
+    </div>
+  </el-drawer>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+.button-hamburger {
+  transition: transform 200ms ease-out 100ms;
+
+  &:hover {
+    cursor: pointer;
+    transform: scale(1.2);
+  }
+}
 
 </style>
